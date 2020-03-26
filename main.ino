34cd57ae49a87bbb4528d15c9161f8299ea67e31
Shirakami Emmmer <yjhdandan@163.com>
2020-03-26 21:53:35 +0800

    ready music

--- main.ino
+++ main.ino
@@ -1,10 +1,46 @@
 #include <Arduino.h>
 
+#define downDo 261
+#define downRe 293
+#define downMi 329
+#define downFa 349
+#define downSo 391
+#define downRa 440
+#define downXi 493
+
+#define midDo 523
+#define midRe 587
+#define midMi 659
+#define midFa 698
+#define midSo 783
+#define midRa 880
+#define midXi 987
+
+#define upDo 1046
+#define upRe 1174
+#define upMi 1318
+#define upFa 1396
+#define upSo 1567
+#define upRa 1760
+#define upXi 1975
+
+int pinBuzzer = 10;
+void sayTone(int tones, int time) {
+    tone(pinBuzzer, tones);
+    delay(time);
+    noTone(pinBuzzer);
+}
+
 void setup() {
-    // TODO: your setup code goes here
+    pinMode(pinBuzzer, OUTPUT);
 }
 
 void loop() {
-    // TODO: rolling the program!
-    printf("123456789");
+    sayTone(midDo, 500);
+    sayTone(midRe,500);
+    sayTone(midMi,500);
+    sayTone(midRe,500);
+
+
 }
+
